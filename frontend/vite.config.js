be6d60d9ce33a7924503c31de101f0e266807518
Andrew Van Os <andrew@Andrews-MacBook-Air.local>
2025-12-06 15:34:49 -0700

    Allow production host for Vite preview

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,5 +5,10 @@ export default defineConfig({
     server: {
         host: '0.0.0.0',
         port: 5173
+    },
+    preview: {
+        host: '0.0.0.0',
+        port: 5173,
+        allowedHosts: ['historicalfacts.interestingalpacafacts.com']
     }
 });
