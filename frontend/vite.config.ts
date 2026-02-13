0d115722912b0afd011066d0032fe2fb02b19c48
Andrew Van Os <andrew@Andrews-MacBook-Air.local>
2026-02-13 16:10:04 -0700

    Allow stravafacts.andvos.xyz in Vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,11 +6,11 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
-    allowedHosts: ['historicalfacts.interestingalpacafacts.com']
+    allowedHosts: ['stravafacts.andvos.xyz']
   },
   preview: {
     host: '0.0.0.0',
     port: 5173,
-    allowedHosts: ['historicalfacts.interestingalpacafacts.com']
+    allowedHosts: ['stravafacts.andvos.xyz']
   }
 });
